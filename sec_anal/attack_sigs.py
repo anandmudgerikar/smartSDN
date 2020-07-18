6f91962845b9203bfd9d27a11e7fc79c3045a7c3
Anand <anandmudgerikar@gmail.com>
2020-07-18 02:02:17 -0400

    bug fixes

--- sec_anal/attack_sigs.py
+++ sec_anal/attack_sigs.py
@@ -1,5 +1,6 @@
 import dpkt
 import ipaddress
+import datetime
 
 f = open('/home/anand/ids_dataset/new_wed.pcap','rb')
 pcap = dpkt.pcap.Reader(f)
@@ -11,32 +12,37 @@ pckt_count_back = 0
 bytes_forward = 0
 bytes_back = 0
 
+#parameters
+STATE_INTERVAL = 10
+user_ips = {'192.168.10.19','192.168.10.14','192.168.10.9','192.168.10.25'}
+attacker_ips = {'172.16.0.1'}
+
 for ts, buf in pcap:
- #maintaining window of 10 secs
- if(ts > prev_ts + 10):
-  prev_ts = ts
-  if(pckt_count_forward > 0 or pckt_count_back > 0):
-   print(pckt_count_forward,bytes_forward,pckt_count_back,bytes_back)
-  pckt_count_forward = 0
-  pckt_count_back = 0
-  bytes_forward = 0
-  bytes_back = 0
+    #maintaining window of 10 secs
+    if(ts > prev_ts + STATE_INTERVAL):
+        prev_ts = ts
+        if(pckt_count_forward > 0 or pckt_count_back > 0):
+            print(str(datetime.datetime.utcfromtimestamp(ts)),pckt_count_forward,bytes_forward,pckt_count_back,bytes_back)
+        pckt_count_forward = 0
+        pckt_count_back = 0
+        bytes_forward = 0
+        bytes_back = 0
 
- eth = dpkt.ethernet.Ethernet(buf)
- ip = eth.data
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
 
- if not isinstance(eth.data, dpkt.ip.IP):
-  #print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
-  continue
+    if not isinstance(eth.data, dpkt.ip.IP):
+        #print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+        continue
 
- #print(ipaddress.ip_address(ip.src), ipaddress.ip_address(ip.dst))
+    #print(ipaddress.ip_address(ip.src), ipaddress.ip_address(ip.dst))
+    for user_ip in attacker_ips:
+        if(ipaddress.ip_address(ip.dst) == ipaddress.ip_address('192.168.10.50') and ipaddress.ip_address(ip.src) == ipaddress.ip_address(user_ip)):
+            pckt_count_forward +=1
+            bytes_forward += len(ip)
 
- if(ipaddress.ip_address(ip.dst) == ipaddress.ip_address('192.168.10.50') and ipaddress.ip_address(ip.src) == ipaddress.ip_address('172.16.0.10')):
-  pckt_count_forward +=1
-  bytes_forward += len(ip)
-
- if(ipaddress.ip_address(ip.src) == ipaddress.ip_address('192.168.10.50') and ipaddress.ip_address(ip.dst) == ipaddress.ip_address('172.16.0.10')):
-  pckt_count_back +=1
-  bytes_back += len(ip)
+        if(ipaddress.ip_address(ip.src) == ipaddress.ip_address('192.168.10.50') and ipaddress.ip_address(ip.dst) == ipaddress.ip_address(user_ip)):
+            pckt_count_back +=1
+            bytes_back += len(ip)
 
 f.close()
