21407959ec5ee6aa31ab66a572f78b32b97f9759
Anand <anandmudgerikar@gmail.com>
2020-07-23 14:16:55 -0400

    robustness testing for attack sigs

---
+++ sec_anal/attack_sigs_notime.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import pickle
+from sklearn import preprocessing
+
+
+# importing Dataset (from pcap parser: known attacks)
+def importdata():
+    balance_data = pd.read_csv("/home/anand/Dropbox/projects/thesis/smart_sdn/sec_anal/state_based/test1_2sec.csv",sep=',', header=0)
+
+    # # removing na and normalizing with mean
+    # balance_data.fillna(balance_data.mean())
+    #
+    #for partial signatures (limited feature set)
+    feature_cols = ['pckts_forward','bytes_forward', 'pckts_back', 'bytes_back', 'label']
+    balance_data = balance_data[feature_cols]  # Features
+
+    # Printing the dataswet shape
+    print("Dataset Length: ", len(balance_data))
+    print("Dataset Shape: ", balance_data.shape)
+    print("Column Names:", balance_data.columns)
+
+    # Printing the dataset obseravtions
+    print("Dataset: ", balance_data.head())
+
+    return balance_data
+
+# split the dataset
+def splitdataset(balance_data):
+    # Separating the target variable
+    X = balance_data.values[:, 0:4]
+    Y = balance_data.values[:, 4]
+
+    #normalizing nan values to max float32
+    X = np.nan_to_num(X.astype(np.float32))
+
+    # #transforming Y to categorical label (0,1)
+    # le = preprocessing.LabelEncoder()
+    # le.fit(["BENIGN", "Malicious"])
+    # Y = le.transform(Y)
+
+    # Splitting the dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.3, random_state=1)
+
+    print("Splitting complete")
+
+    if(balance_data.isna == True):
+        print("error: nan infinity in the dataset")
+
+    return X, Y, X_train, X_test, y_train, y_test
+
+
+def train(X_train, y_train):
+    # Creating the Decision Tree classifier object
+    dec_tree = DecisionTreeClassifier()
+
+    # Performing training
+    dec_tree.fit(X_train, y_train)
+    print("Training complete")
+
+    #save dec tree model
+    filename = 'partial_sig.sav'
+    pickle.dump(dec_tree, open(filename, 'wb'))
+    return dec_tree
+
+# Function to make predictions
+def prediction(X_test, clf_object):
+    # Predicton on test with giniIndex
+    y_pred = clf_object.predict(X_test)
+    print("Predicted values:")
+    #print(y_pred)
+    return y_pred
+
+
+# Function to calculate accuracy
+def cal_accuracy(y_test, y_pred):
+    print("Confusion Matrix: ",
+          confusion_matrix(y_test, y_pred))
+
+    print("Accuracy : ",
+          accuracy_score(y_test, y_pred) * 100)
+
+    print("Report : ",
+          classification_report(y_test, y_pred))
+
+
+# Driver code
+def main():
+    # Building Phase
+    data = importdata()
+    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    dec_tree = train(X_train,y_train)
+
+    # Operational Phase
+    print("Results:")
+
+    #adding noise for robustness testing
+    noise = np.random.normal(0, 10, X_test.shape)
+    X_test += noise.round().astype(int)
+
+    print(X_test)
+
+    # Prediction
+    y_pred = prediction(X_test, dec_tree)
+    cal_accuracy(y_test, y_pred)
+
+# Calling main function
+if __name__ == "__main__":
+    main()
--- sec_anal/attack_sigs_pcap_parser.py
+++ sec_anal/attack_sigs_pcap_parser.py
@@ -10,6 +10,20 @@ for file in glob.glob("*.pcap"):
 
     f = open(file,'rb')
     pcap = dpkt.pcap.Reader(f)
+    #print(file)
+
+    if("Thursday" in file):
+        label = "web"
+    elif("Wednesday" in file):
+        label = "dos"
+    elif("Tuesday" in file):
+        label = "bruteforce"
+    elif("Friday" in file):
+        label = "ddos"
+    else:
+        label = "unknown"
+
+    # print(label)
 
     reset = 0
     prev_ts = 0
@@ -19,7 +33,7 @@ for file in glob.glob("*.pcap"):
     bytes_back = 0
 
     #parameters
-    STATE_INTERVAL = 2
+    STATE_INTERVAL = 10
     user_ips = {'192.168.10.19','192.168.10.14','192.168.10.9','192.168.10.25'}
     attacker_ips = {'172.16.0.1'}
 
@@ -28,7 +42,7 @@ for file in glob.glob("*.pcap"):
         if(ts > prev_ts + STATE_INTERVAL):
             prev_ts = ts
             if(pckt_count_forward > 0 or pckt_count_back > 0):
-                print(str(datetime.datetime.utcfromtimestamp(ts)),pckt_count_forward,bytes_forward,pckt_count_back,bytes_back)
+                print(str(datetime.datetime.utcfromtimestamp(ts)),pckt_count_forward,bytes_forward,pckt_count_back,bytes_back,label)
             pckt_count_forward = 0
             pckt_count_back = 0
             bytes_forward = 0
@@ -42,7 +56,7 @@ for file in glob.glob("*.pcap"):
             continue
 
         #print(ipaddress.ip_address(ip.src), ipaddress.ip_address(ip.dst))
-        for user_ip in user_ips:
+        for user_ip in attacker_ips:
             if(ipaddress.ip_address(ip.dst) == ipaddress.ip_address('192.168.10.50') and ipaddress.ip_address(ip.src) == ipaddress.ip_address(user_ip)):
                 pckt_count_forward +=1
                 bytes_forward += len(ip)
